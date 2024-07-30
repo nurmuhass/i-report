6f1efe01fdcfe3b7b45e64d6b99dc32175f49fd1
Nurmuhass <nurmuhass@gmail.com>
2024-07-30 13:41:40 +0100

    finished login ui and backend

--- app/(auth)/create-account.js
+++ app/(auth)/create-account.js
@@ -1,8 +1,13 @@
-import { Text, View, TextInput, StyleSheet } from "react-native";
+import { Text, View, TextInput, StyleSheet, StatusBar } from "react-native";
 import { useRef } from "react";
 import { AuthStore, appSignUp } from "../../store.js";
 import { Stack, useRouter } from "expo-router";
 import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
+import Feather from '@expo/vector-icons/Feather';
+import Fontisto from '@expo/vector-icons/Fontisto';
+import { Button } from "@rneui/themed";
 
 export default function CreateAccount() {
   const router = useRouter();
@@ -13,59 +18,76 @@ export default function CreateAccount() {
 
   return (
     <View style={{ flex: 1,backgroundColor:'#ffffff' }}>
+  <StatusBar
+    translucent
+    barStyle="dark-content"
+    backgroundColor="rgba(255, 255, 255, 0)" // Transparent white color
+/>
        <View style={{marginLeft:15 }}>
       <Stack.Screen
         options={{ title: "Create Account", headerLeft: () => <></> }}
       />
 
-
-<View style={{backgroundColor:'grey',borderRadius:5,width:28,marginTop:30}}>
-  <Ionicons name="chevron-back" size={22} color="black" />
-</View>
+<TouchableOpacity style={{backgroundColor:'#f0f0f0',borderRadius:5,width:30,
+    marginTop:40,height:28,alignItems:'center',justifyContent:'center'}} onPress={router.back}>
+    <Ionicons name="chevron-back" size={18} color="black" />
+  </TouchableOpacity>
 
 <View style={{marginTop:20}}>
   <Text style={{fontSize:32,fontWeight:'bold'}}>Let's </Text>
   <Text style={{fontSize:32,fontWeight:'bold'}}>Get Started</Text>
 </View>
-<Text style={{marginTop:40}}>Please fill the details to create an account</Text>
+<Text style={{marginTop:40,color:'#555',fontSize:15}}>Please fill the details to create an account</Text>
 
-      <View style={{marginTop:10}}>
-        <Text style={styles.label}>Email</Text>
+
+<View style={{justifyContent:'center',marginTop:15}}>
+
+
+<View style={{alignContent:'center'}}>
         <TextInput
-          placeholder="email"
+          placeholder="Enter Your Email"
           nativeID="email"
           onChangeText={(text) => {
             emailRef.current = text;
           }}
-          style={styles.textInput}
+          style={{...styles.textInput}}
         />
-      </View>
+      <Fontisto name="email" size={18} color="#555" style={styles.icon} />
+        
+</View>
+
       <View>
-        <Text style={styles.label}>First Name</Text>
+       
         <TextInput
-          placeholder="firstName"
+          placeholder="Enter Your FirstName"
           nativeID="firstName"
           onChangeText={(text) => {
             firstNameRef.current = text;
           }}
           style={styles.textInput}
         />
+
+<FontAwesome5 name="user" size={18} color="#555"  style={styles.icon}/>
       </View>
+
+
       <View>
-        <Text style={styles.label}>Last Name</Text>
+   
         <TextInput
-          placeholder="lastName"
+          placeholder="Enter Your LastName"
           nativeID="lastName"
           onChangeText={(text) => {
             lastNameRef.current = text;
           }}
           style={styles.textInput}
         />
+
+<FontAwesome5 name="user" size={18} color="#555"  style={styles.icon}/>
       </View>
       <View>
-        <Text style={styles.label}>Password</Text>
+       
         <TextInput
-          placeholder="password"
+          placeholder="Enter Your Password"
           secureTextEntry={true}
           nativeID="password"
           onChangeText={(text) => {
@@ -73,10 +95,14 @@ export default function CreateAccount() {
           }}
           style={styles.textInput}
         />
+        <Feather name="lock" size={18} color="#555"  style={styles.icon}/>
+      </View>
       </View>
 
-      <Text
-        style={{ marginBottom: 8 }}
+      <Button
+      buttonStyle={{marginTop:10,width:'90%',padding:15,
+        backgroundColor:'#00C26F',color:'#fff',borderRadius:20,shadowColor:'#3E3E3E',shadowOffset:{width:0,height:10},shadowOpacity:0.2,shadowRadius:8,elevation:4}}
+
         onPress={async () => {
           const resp = await appSignUp(
             emailRef.current,
@@ -91,19 +117,22 @@ export default function CreateAccount() {
           }
         }}
       >
-        SAVE NEW USER
-      </Text>
-
-      <Text
+        Sign up
+      </Button>
+<View style={{marginTop:30,flexDirection:'row',alignItems:'center',justifyContent:'center'}}>
+  <Text>Already have an account!</Text>
+<Text
         onPress={() => {
           AuthStore.update((s) => {
             s.isLoggedIn = false;
           });
           router.back();
-        }}
+        }} style={{color:'#00C26F',marginLeft:3}}
       >
-        CANCEL
+        Login
       </Text>
+</View>
+
       </View>
     </View>
   );
@@ -115,12 +144,16 @@ const styles = StyleSheet.create({
     color: "#455fff",
   },
   textInput: {
-    width: 250,
+    width: '90%',
     borderWidth: 1,
-    borderRadius: 4,
-    borderColor: "#455fff",
+    borderRadius: 18,
+    borderColor: "#555",
     paddingHorizontal: 8,
-    paddingVertical: 4,
-    marginBottom: 8,
+    paddingVertical: 10,
+    marginBottom: 15,
+    paddingLeft:40
   },
+  icon:{
+    position:'absolute',left:10,top:15
+  }
 });
--- app/(auth)/login.js
+++ app/(auth)/login.js
@@ -2,6 +2,14 @@ import { Text, View, TextInput, StyleSheet, Alert } from "react-native";
 import { AuthStore, appSignIn } from "../../store.js";
 import { Stack, useRouter } from "expo-router";
 import { useRef } from "react";
+import { StatusBar } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
+import Feather from '@expo/vector-icons/Feather';
+import Fontisto from '@expo/vector-icons/Fontisto';
+import { Button } from "@rneui/themed";
+import { TouchableOpacity } from "react-native";
 
 export default function LogIn() {
   const router = useRouter();
@@ -9,70 +17,122 @@ export default function LogIn() {
   const passwordRef = useRef("");
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <View>
-        <Text style={styles.label}>Email</Text>
-        <TextInput
-          placeholder="email"
-          autoCapitalize="none"
-          nativeID="email"
-          onChangeText={(text) => {
-            emailRef.current = text;
-          }}
-          style={styles.textInput}
+<>
+    <View style={{ flex: 1,backgroundColor:'#ffffff' }}>
+    <StatusBar
+      translucent
+      barStyle="dark-content"
+      backgroundColor="rgba(255, 255, 255, 0)" // Transparent white color
+  />
+         <View style={{marginLeft:15 }}>
+        <Stack.Screen
+          options={{ title: "Create Account", headerLeft: () => <></> }}
         />
-      </View>
-      <View>
-        <Text style={styles.label}>Password</Text>
-        <TextInput
-          placeholder="password"
-          secureTextEntry={true}
-          nativeID="password"
-          onChangeText={(text) => {
-            passwordRef.current = text;
+  
+  
+  <TouchableOpacity style={{backgroundColor:'#f0f0f0',borderRadius:5,width:30,
+    marginTop:40,height:28,alignItems:'center',justifyContent:'center'}} onPress={router.back}>
+    <Ionicons name="chevron-back" size={18} color="black" />
+  </TouchableOpacity>
+  
+  <View style={{marginTop:20}}>
+    <Text style={{fontSize:32,fontWeight:'bold'}}>Hey, </Text>
+    <Text style={{fontSize:32,fontWeight:'bold'}}>Welcome Back</Text>
+  </View>
+  <Text style={{marginTop:40,color:'#555',fontSize:15}}>Please login to continue</Text>
+  
+  
+  <View style={{justifyContent:'center',marginTop:15}}>
+  
+  
+  <View style={{alignContent:'center',}}>
+          <TextInput
+            placeholder="Enter Your Email"
+            nativeID="email"
+            onChangeText={(text) => {
+              emailRef.current = text;
+            }}
+            style={{...styles.textInput}}
+          />
+
+        <Fontisto name="email" size={18} color="#555" style={styles.icon} />
+          
+  </View>
+  
+
+   <View>
+         
+          <TextInput
+            placeholder="Enter Your Password"
+            secureTextEntry={true}
+            nativeID="password"
+            onChangeText={(text) => {
+              passwordRef.current = text;
+            }}
+            style={styles.textInput}
+          />
+          <Feather name="lock" size={18} color="#555"  style={styles.icon}/>
+   </View>
+        </View>
+  
+<View>
+               <Text style={{marginRight:5,fontWeight:'bold',fontSize:14,marginLeft:'65%'}}>Forget Password?</Text>
+</View>
+       
+
+        <Button
+        buttonStyle={{marginTop:10,width:'90%',padding:15,
+          backgroundColor:'#00C26F',color:'#fff',borderRadius:20,shadowColor:'#3E3E3E',shadowOffset:{width:0,height:10},shadowOpacity:0.2,shadowRadius:8,elevation:4}}
+  
+          onPress={async () => {
+            const resp = await appSignIn(emailRef.current, passwordRef.current);
+            if (resp?.user) {
+              router.replace("/(tabs)/home");
+            } else {
+              console.log(resp.error)
+              Alert.alert("Login Error", resp.error?.message)
+            }
           }}
-          style={styles.textInput}
-        />
+        >
+          Login
+        </Button>
+
+
+  <View style={{marginTop:30,flexDirection:'row',alignItems:'center',justifyContent:'center'}}>
+    <Text>Don't have an account!</Text>
+  <Text
+                 onPress={() => {
+                  AuthStore.update((s) => {
+                    s.isLoggedIn = true;
+                  });
+                  router.push("/create-account");
+                }} style={{color:'#00C26F',marginLeft:3}}
+        >
+         Sign up
+        </Text>
+  </View>
+  
+        </View>
       </View>
-      <Text
-        onPress={async () => {
-          const resp = await appSignIn(emailRef.current, passwordRef.current);
-          if (resp?.user) {
-            router.replace("/(tabs)/home");
-          } else {
-            console.log(resp.error)
-            Alert.alert("Login Error", resp.error?.message)
-          }
-        }}
-      >
-        Login
-      </Text>
-      <Text
-        onPress={() => {
-          AuthStore.update((s) => {
-            s.isLoggedIn = true;
-          });
-          router.push("/create-account");
-        }}
-      >
-        Create Account
-      </Text>
-    </View>
+
+
+
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  label: {
-    marginBottom: 4,
-    color: "#455fff",
-  },
   textInput: {
-    width: 250,
+    width: '92%',
     borderWidth: 1,
-    borderRadius: 4,
-    borderColor: "#455fff",
+    borderRadius: 18,
+    borderColor: "#555",
     paddingHorizontal: 8,
-    paddingVertical: 4,
-    marginBottom: 8,
+    paddingVertical: 10,
+    marginBottom: 15,
+    paddingLeft:40
   },
+  icon:{
+    position:'absolute',left:10,top:15
+  }
 });
